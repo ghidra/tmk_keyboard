915879ff6f1224e0741555cfcc99cdd443a83d0b
jimmy <ghidra@gmail.com>
2016-08-04 02:08:05 -0400

    updated keymap

--- keyboard/gh60/keymap_poker.c
+++ keyboard/gh60/keymap_poker.c
@@ -6,13 +6,13 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS,\
         CAPS,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     ENT,\
         LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,          RSFT,\
-        LCTL,LGUI,LALT,          SPC,                RALT,PAUS,     FN0, RCTL),
+        LCTL,LGUI,LALT,          SPC,                RALT,PAUS,    RCTL,FN0),
     /* 0: function 1 */
     KEYMAP(MUTE, F1,   F2,   F3,   F4,   F5,   F6,   F7,   F8,   F9,   F10,   F11,F12, DEL, \
            VOLU, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS, \
            VOLD,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     ENT,  \
-           LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,          RSFT, \
-           LCTL,LGUI,LALT,          SPC,                RALT,PAUS,     FN0, RCTL),
+           LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, UP,          RSFT, \
+           LCTL,LGUI,LALT,          SPC,                LEFT,DOWN,     RIGHT, FN0),
 };
 const uint16_t PROGMEM fn_actions[] = {
     [0] = ACTION_LAYER_MOMENTARY(1),
