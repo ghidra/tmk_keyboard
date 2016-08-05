30cbcef5f04eb24ea536b1ff185c329d18240073
jimmy <ghidra@gmail.com>
2016-08-04 21:29:10 -0400

    updated second layer keymapping

--- keyboard/gh60/keymap_poker.c
+++ keyboard/gh60/keymap_poker.c
@@ -6,13 +6,13 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS,\
         CAPS,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     ENT,\
         LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,          RSFT,\
-        LCTL,LGUI,LALT,          SPC,                RALT,PAUS,    RCTL,FN0),
+        LCTL,LGUI,LALT,          SPC,                RALT,PAUS,     FN0, SPC ),
     /* 0: function 1 */
-    KEYMAP(MUTE, F1,   F2,   F3,   F4,   F5,   F6,   F7,   F8,   F9,   F10,   F11,F12, DEL, \
-           VOLU, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS, \
-           VOLD,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     ENT,  \
-           LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, UP,          RSFT, \
-           LCTL,LGUI,LALT,          SPC,                LEFT,DOWN,     RIGHT, FN0),
+    KEYMAP( GRV,   F1,   F2,   F3,   F4,   F5,   F6,   F7,   F8,   F9,  F10,  F11,  F12,  DEL, \
+           MUTE, TRNS,   UP, TRNS, TRNS, TRNS, TRNS,   P7,   P8,   P9, TRNS, TRNS, TRNS, PSCR, \
+           VOLU, LEFT, DOWN,RIGHT, TRNS, TRNS, TRNS,   P4,   P5,   P6, SCLN, QUOT,       HOME, \
+           VOLD, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,   P1,   P2,   P3, PGUP,              END, \
+           LSFT, TRNS, TRNS, TRNS,               P0,                   PGDN,       TRNS, TRNS),
 };
 const uint16_t PROGMEM fn_actions[] = {
     [0] = ACTION_LAYER_MOMENTARY(1),
